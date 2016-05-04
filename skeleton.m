dff07f09bd04acdc262c27de8b5471b69b320a64
Pietro Ferretti <pietro.ferretti1@gmail.com>
2016-05-04 13:53:12 +0200

    Added denormalization and plot tweaks
    I merged both versions of the file, it should work.
    * added plots for single test cases (like nCores)

--- skeleton.m
+++ skeleton.m
@@ -1,7 +1,6 @@
 %% Skeleton
 
 %%% TODO
-%% - Rescaling features to have meaningful plots
 %% - Automatic feature selection (with penalties?)
 
 
@@ -10,6 +9,7 @@ clc;
 close all hidden;
 
 BASE_DIR = '/home/peter/MachineLearning/dati/';
+% BASE_DIR = '/Users/Andrea/Documents/ProgettoML/dati/';
 
 %% List of all directories with train data
 TRAIN_DATA_LOCATION = {'Query R/R2/Core/60','Query R/R2/Core/72','Query R/R2/Core/90','Query R/R2/Core/100','Query R/R2/Core/80'};
@@ -19,6 +19,18 @@ TRAIN_DATA_LOCATION = {'Query R/R2/Core/60','Query R/R2/Core/72','Query R/R2/Cor
 %% List of all directories with test data (leave {} if test data equals train data)
 TEST_DATA_LOCATION = {'Query R/R2/Core/120'};
 %TEST_DATA_LOCATION = {};
+
+SAVE_PLOTS = true;
+OUTPUT_PLOTS_LOCATION = 'outputPlots/';
+OUTPUT_FORMATS = {	{'-deps', '.eps'},					% generates only one .eps file black and white
+					{'-depslatex', '.eps'},				% generates one .eps file containing only the plot and a .tex file that includes the plot and fill the legend with plain text
+					{'-depsc', '.eps'},					% generates only one .eps file with colour
+					{'-dpdflatex', '.pdf'}				% generates one .pdf file containing only the plot and a .tex file that includes the plot and fill the legend with plain text
+					{'-dpdf', '.pdf'}					% generates one complete .pdf file A4
+				};
+PLOT_SAVE_FORMAT = 3;
+
+
 
 %% CHANGE THESE IF TEST == TRAIN
 TRAIN_FRAC_WO_TEST = 0.6;
@@ -48,8 +60,9 @@ CLEAR_OUTLIERS = true;
 % 13 -> N Core
 CHOOSE_FEATURES = true;
 
-FEATURES = [1:8, 12, 13]; 			% All the features except Users and Bandwidth
-% FEATURES = [1, 2, 7, 12, 13];
+FEATURES = [1:8, 12:13]; 			% All the features except Users and Bandwidth
+% NB: Bandwidth crea problemi con la linear regression, perché i valori sono tutti uguali per alcuni test
+% LINEAR_REGRESSION va messa a false in quei casi
 
 FEATURES_DESCRIPTIONS = {			% These will be used to describe the plot axis
 	'N map',
@@ -72,7 +85,7 @@ FEATURES_DESCRIPTIONS = {			% These will be used to describe the plot axis
 % 2 -> Polynomial SVR
 % 3 -> RBF SVR
 MODELS_CHOSEN = [1, 2, 3];
-COLORS = {'g', 'b', 'c'};
+COLORS = {'m', [1, 0.5, 0], 'c'};	% magenta, orange, cyan
 
 LINEAR_REGRESSION = true;
 
@@ -94,7 +107,8 @@ E_range = linspace (0.1, 5, 20);
 train_data = get_all_data_from_dirs(BASE_DIR, TRAIN_DATA_LOCATION);
 
 if CHOOSE_FEATURES
-	train_data = [train_data(:, 1) , train_data(:, 2:end)(:, FEATURES)];
+	tmp = train_data(:, 2:end);
+	train_data = [train_data(:, 1) , tmp(:, FEATURES)];
 	FEATURES_DESCRIPTIONS = FEATURES_DESCRIPTIONS(FEATURES);
 end
 
@@ -102,7 +116,8 @@ test_data = [];
 if not (isempty(TEST_DATA_LOCATION))
 	test_data = get_all_data_from_dirs(BASE_DIR, TEST_DATA_LOCATION);
 	if CHOOSE_FEATURES
-		test_data = [test_data(:, 1) , test_data(:, 2:end)(:, FEATURES)];
+		tmp = test_data(:, 2:end);
+		test_data = [test_data(:, 1) , tmp(:, FEATURES)];
 	end
 end
 
@@ -216,7 +231,7 @@ models = {};
 %% White box model, nCores  LINEAR
 if ismember(1, MODELS_CHOSEN)
 	fprintf('\nTraining model with linear SVR\n');
-	fflush(stdout);
+	%fflush(stdout);
 	SVR_DESCRIPTIONS{end + 1} = 'Linear SVR';
 
 	[C, eps] = model_selection (y_tr, X_tr, y_cv, X_cv, '-s 3 -t 0 -q -h 0', C_range, E_range);
@@ -227,8 +242,8 @@ if ismember(1, MODELS_CHOSEN)
 	sum_abs = 0;
 	sum_rel = 0;
 	for i = 1:N_test
-		sum_abs += abs(y_test(i) - predictions(i, end));
-		sum_rel += abs((y_test(i) - predictions(i, end)) / predictions(i, end));
+		sum_abs = sum_abs + abs(y_test(i) - predictions(i, end));
+		sum_rel = sum_rel + abs((y_test(i) - predictions(i, end)) / predictions(i, end));
 	end
 	mean_abs = sum_abs / N_test;
 	mean_rel = sum_rel / N_test;
@@ -251,7 +266,7 @@ end
 %% Black box model, Polynomial
 if ismember(2, MODELS_CHOSEN)
 	fprintf('\nTraining model with polynomial SVR\n');
-	fflush(stdout);
+	%fflush(stdout);
 	SVR_DESCRIPTIONS{end + 1} = 'Polynomial SVR';
 
 	[C, eps] = model_selection (y_tr, X_tr, y_cv, X_cv, '-s 3 -t 1 -q -h 0', C_range, E_range);
@@ -262,8 +277,8 @@ if ismember(2, MODELS_CHOSEN)
 	sum_abs = 0;
 	sum_rel = 0;
 	for i = 1:N_test
-		sum_abs += abs(y_test(i) - predictions(i, end));
-		sum_rel += abs((y_test(i) - predictions(i, end)) / predictions(i, end));
+		sum_abs = sum_abs + abs(y_test(i) - predictions(i, end));
+		sum_rel = sum_rel + abs((y_test(i) - predictions(i, end)) / predictions(i, end));
 	end
 	mean_abs = sum_abs / N_test;
 	mean_rel = sum_rel / N_test;
@@ -286,7 +301,7 @@ end
 %% Black box model, RBF (Radial Basis Function)
 if ismember(3, MODELS_CHOSEN)
 	fprintf('\nTraining model with RBF SVR\n');
-	fflush(stdout);
+	%fflush(stdout);
 	SVR_DESCRIPTIONS{end + 1} = 'Radial Basis Function SVR';
 
 	[C, eps] = model_selection (y_tr, X_tr, y_cv, X_cv, '-s 3 -t 2 -q -h 0', C_range, E_range);
@@ -297,8 +312,8 @@ if ismember(3, MODELS_CHOSEN)
 	sum_abs = 0;
 	sum_rel = 0;
 	for i = 1:N_test
-		sum_abs += abs(y_test(i) - predictions(i, end));
-		sum_rel += abs((y_test(i) - predictions(i, end)) / predictions(i, end));
+		sum_abs = sum_abs + abs(y_test(i) - predictions(i, end));
+		sum_rel = sum_rel + abs((y_test(i) - predictions(i, end)) / predictions(i, end));
 	end
 	mean_abs = sum_abs / N_test;
 	mean_rel = sum_rel / N_test;
@@ -346,14 +361,14 @@ if LINEAR_REGRESSION
 	sum_abs = 0;
 	sum_rel = 0;
 	for i = 1:N_test
-		sum_residual += (y_test(i) - predictions(i, end))^2;
-		sum_total += (y_test(i) - y_mean)^2;
-		sum_abs += abs(y_test(i) - predictions(i, end));
-		sum_rel += abs((y_test(i) - predictions(i, end)) / predictions(i, end));
+		sum_residual = sum_residual + (y_test(i) - predictions(i, end))^2;
+		sum_total = sum_total + (y_test(i) - y_mean)^2;
+		sum_abs = sum_abs + abs(y_test(i) - predictions(i, end));
+		sum_rel = sum_rel + abs((y_test(i) - predictions(i, end)) / predictions(i, end));
 	end
 
-	lin_RMSE = sqrt(sum_residual / N_test);	%% Root Mean Squared Error
-	lin_R2 = 1 - (sum_residual / sum_total);		%% R^2
+	lin_RMSE = sqrt(sum_residual / N_test);			% Root Mean Squared Error
+	lin_R2 = 1 - (sum_residual / sum_total);		% R^2
 	lin_mean_abs = sum_abs / N_test;
 	lin_mean_rel = sum_rel / N_test;
 
@@ -383,23 +398,56 @@ for col = 1:M
 	figure;
 	hold on;
 
-	% Scatters training and test data
-	scatter(X_tr(:, col), y_tr, 'r', 'x');
-	scatter(X_test(:, col), y_test, 'b');
+	% Denormalizes and scatters training and test data
+	X_tr_denorm(:, col) = (X_tr(:, col) * sigma_X(col)) + mu_X(col);
+	y_tr_denorm = (y_tr * sigma_y) + mu_y;
+	X_test_denorm(:, col) = (X_test(:, col) * sigma_X(col)) + mu_X(col);
+	y_test_denorm = (y_test * sigma_y) + mu_y;
+
+	scatter(X_tr_denorm(:, col), y_tr_denorm, 'r', 'x');
+	scatter(X_test_denorm(:, col), y_test_denorm, 'b');
 
 	% w = SVs{svr_index}' * coefficients{svr_index};
-	x = linspace(min(X_test(:, col)), max(X_test(:, col)));
-	xsvr = zeros(length(x), M);
-	xsvr(:, col) = x;
+	x = linspace(min(X_test(:, col)), max(X_test(:, col)));		% Normalized, we need this for the predictions
+	x_denorm = (x * sigma_X(col)) + mu_X(col);
+
+	xsvr = zeros(length(x), M);			% xsvr is a matrix of zeros, except for the column we're plotting currently
+	xsvr(:, col) = x;					% It must be normalized to use svmpredict with the SVR models we found
 
 	% plot(x, w(col)*x + b{svr_index}, 'g');
 	if LINEAR_REGRESSION
-		plot(x, x*theta(col+1), 'm', 'linewidth', 1);
+		ylin = x * theta(col+1);
+
+		% Denormalize y
+		if NORMALIZE_FEATURE
+			ylin = (ylin * sigma_y) + mu_y;
+		end
+
+		if (x(1) == x(end))
+			scatter(x_denorm(1), ylin(1), 10, 'g', 'd', 'filled');		% Plot single points (for nCores)
+		else
+			plot(x_denorm, ylin, 'g', 'linewidth', 1);
+		end
 	end
 
 	for index = 1:length(MODELS_CHOSEN)
 		[ysvr, ~, ~] = svmpredict(zeros(length(x), 1), xsvr, models{index}, '-q');	%% quiet
-		plot(x, ysvr, COLORS{index}, 'linewidth', 1);
+
+		% Denormalize
+		if NORMALIZE_FEATURE
+			ysvr = (ysvr * sigma_y) + mu_y;
+		end 
+
+		if (x(1) == x(end))
+			scatter(x_denorm(1), ysvr(1), 10, COLORS{index}, 'd', 'filled');		% Plot single points (for nCores)
+		else	
+			plot(x_denorm, ysvr, 'color', COLORS{index}, 'linewidth', 1);
+		end
+	end
+
+	% Plot the mean of the expected values (for nCores)
+	if(x(1) == x(end))
+		scatter(x_denorm(1), mean(y_test_denorm), 10, 'k', '.');		
 	end
 	
 	labels = {'Training set', 'Testing set'};
@@ -414,11 +462,16 @@ for col = 1:M
 	% plot(x, w(col)*x + b{svr_index} - Es(svr_index), 'k');
 
 	% Labels the axes
-	xlabel(FEATURES_DESCRIPTIONS(col));
+	xlabel(FEATURES_DESCRIPTIONS{col});
 	ylabel('Completion Time');
 	% title(cstrcat('Linear regression vs ', SVR_DESCRIPTIONS{svr_index})); 
-	
+	if SAVE_PLOTS
+		% NOTE: the file location shouldn't have any spaces
+		file_location = strrep(strcat(OUTPUT_PLOTS_LOCATION, 'plot_', FEATURES_DESCRIPTIONS{col}, OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{2}), ' ', '');
+		print(OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{1}, file_location);
+	end
 	hold off;
+	
 	% pause;
 
 end
