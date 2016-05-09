114810ece284e5696da65f7aff468c5eef1b42ed
Andrea <andrea.battistello1994@gmail.com>
2016-05-09 10:42:35 +0200

    added learning curves and save plot variables

--- skeleton.m
+++ skeleton.m
@@ -13,15 +13,28 @@ addpath('./utility/');
 BASE_DIR = './dati/';
 
 %% List of all directories with train data
-TRAIN_DATA_LOCATION = {'Query R/R2/Core/60','Query R/R2/Core/72','Query R/R2/Core/90','Query R/R2/Core/100','Query R/R2/Core/120'};
+TRAIN_DATA_LOCATION = {'Query R/R1/Core/60','Query R/R1/Core/72','Query R/R1/Core/90','Query R/R1/Core/100','Query R/R1/Core/80'};
 % TRAIN_DATA_LOCATION = {'Core/60', 'Core/80', 'Core/100', 'Core/120', 'Core/72'};
+% TRAIN_DATA_LOCATION = {'Query R/R1/Datasize/750'};
 
 %% List of all directories with test data (leave {} if test data equals train data)
-TEST_DATA_LOCATION = {'Query R/R2/Core/80'};
-%TEST_DATA_LOCATION = {};
+TEST_DATA_LOCATION = {'Query R/R1/Core/120'};
+TEST_DATA_LOCATION = {};
+
+OUTPUT_LATEX = true;
+QUERY = 'R1';
+DATASIZE = '750';
+if OUTPUT_LATEX
+	TRAIN_DATA_LOCATION = {strcat('Query R/', QUERY, '/Datasize/', DATASIZE)};	% Watch out, overrides TRAIN_DATA_LOCATION
+end
 
 SAVE_DATA = true;
-OUTPUT_FOLDER = 'outputPlots/R2/72CORE_VS_ALL/';
+
+if OUTPUT_LATEX
+	OUTPUT_FOLDER = strcat('output/', QUERY, '_', DATASIZE, '/');
+else
+	OUTPUT_FOLDER = 'output/';
+end
 OUTPUT_FORMATS = {	{'-deps', '.eps'},					% generates only one .eps file black and white
 					{'-depslatex', '.eps'},				% generates one .eps file containing only the plot and a .tex file that includes the plot and fill the legend with plain text
 					{'-depsc', '.eps'},					% generates only one .eps file with colour
@@ -30,14 +43,8 @@ OUTPUT_FORMATS = {	{'-deps', '.eps'},					% generates only one .eps file black a
 				};
 PLOT_SAVE_FORMAT = 3;
 
-
-
-
-ENABLE_FEATURE_FILTERING = true;
-COMPLETION_TIME_THRESHOLD = 400000;
-
-
-
+ENABLE_FEATURE_FILTERING = false;
+COMPLETION_TIME_THRESHOLD = 120000;
 
 
 %% CHANGE THESE IF TEST == TRAIN
@@ -51,6 +58,9 @@ TRAIN_FRAC_W_TEST = 0.7;
 NON_LINEAR_FEATURES = false;
 NORMALIZE_FEATURE = true;
 CLEAR_OUTLIERS = true;
+
+
+LEARNING_CURVES = true;
 
 %% FEATURE DESCRIPTION:
 % 1 -> N map
@@ -68,7 +78,8 @@ CLEAR_OUTLIERS = true;
 % 13 -> N Core
 CHOOSE_FEATURES = true;
 
-FEATURES = [1:8, 12:13]; 			% All the features except Users and Bandwidth
+FEATURES = [3:8, 13];
+% FEATURES = [1:10, 12:13]; 
 % NB: Bandwidth crea problemi con la linear regression, perché i valori sono tutti uguali per alcuni test
 % LINEAR_REGRESSION va messa a false in quei casi
 
@@ -97,9 +108,9 @@ COLORS = {'m', [1, 0.5, 0], 'c'};	% magenta, orange, cyan
 
 LINEAR_REGRESSION = true;
 
-TEST_ON_CORES = true;	% To add the "difference between means" metric
+TEST_ON_CORES = false;	% To add the 'difference between means' metric
 
-rand('seed', 24);
+rand('seed', 17);
 SHUFFLE_DATA = true;
 
 C_range = linspace (0.1, 5, 20);
@@ -136,8 +147,10 @@ if ENABLE_FEATURE_FILTERING
 	rows_ok = train_data(:, 1) < COMPLETION_TIME_THRESHOLD;
 	train_data = train_data(rows_ok, :);
 
-	rows_ok = test_data(:, 1) < COMPLETION_TIME_THRESHOLD;
-	test_data = test_data(rows_ok, :);
+	if not (isempty(TEST_DATA_LOCATION))
+		rows_ok = test_data(:, 1) < COMPLETION_TIME_THRESHOLD;
+		test_data = test_data(rows_ok, :);
+	end
 end
 
 
@@ -227,7 +240,6 @@ sigma_X = sigma(2:end);
 
 %% DECLARE USEFUL VARIABLES
 
-RMSEs = [];
 Cs = [];     
 Es = [];
 predictions = [];
@@ -237,7 +249,13 @@ b = {};
 SVR_DESCRIPTIONS = {};
 models = {};
 means = [];
+
+% Saving test metrics
+RMSEs = [];
 R_2 = [];
+MAE = [];	% Errore assoluto medio
+MRE = [];	% Errore relativo medio
+DM = [];	% Differenza tra medie
 
 %% SVR
 
@@ -254,7 +272,7 @@ R_2 = [];
 if ismember(1, MODELS_CHOSEN)
 	fprintf('\nTraining model with linear SVR\n');
 
-	SVR_DESCRIPTIONS{end + 1} = 'Linear SVR';
+	SVR_DESCRIPTIONS{end + 1} = 'SVR lineare';
 
 	[C, eps] = model_selection (y_tr, X_tr, y_cv, X_cv, '-s 3 -t 0 -q -h 0', C_range, E_range);
 	options = ['-s 3 -t 0 -h 0 -p ', num2str(eps), ' -c ', num2str(C)];
@@ -262,6 +280,12 @@ if ismember(1, MODELS_CHOSEN)
 
 	[predictions(:, end + 1), accuracy, ~] = svmpredict (y_test, X_test, model, '-q');  %% quiet
 
+	if LEARNING_CURVES
+		[m, mse_train, mse_test] = learning_curves(y_tr, X_tr, y_test, X_test, [options, ' -q']);
+		h = plot_learning_curves(m, mse_train, mse_test);
+		print('-depsc', cstrcat(OUTPUT_FOLDER, 'learning_curve_', SVR_DESCRIPTIONS{end}, '.eps'));
+		close(h);
+	end
 
 	models{end + 1} = model;
 	Cs(end + 1) = C;
@@ -278,13 +302,20 @@ end
 if ismember(2, MODELS_CHOSEN)
 	fprintf('\nTraining model with polynomial SVR\n');
 	%fflush(stdout);
-	SVR_DESCRIPTIONS{end + 1} = 'Polynomial SVR';
+	SVR_DESCRIPTIONS{end + 1} = 'SVR polinomiale';
 
 	[C, eps] = model_selection (y_tr, X_tr, y_cv, X_cv, '-s 3 -t 1 -q -h 0', C_range, E_range);
 	options = ['-s 3 -t 1 -h 0 -p ', num2str(eps), ' -c ', num2str(C)];
 	model = svmtrain (y_tr, X_tr, options);
 
 	[predictions(:, end + 1), accuracy, ~] = svmpredict (y_test, X_test, model, '-q');  %% quiet
+
+	if LEARNING_CURVES
+		[m, mse_train, mse_test] = learning_curves(y_tr, X_tr, y_test, X_test, [options, ' -q']);
+		h = plot_learning_curves(m, mse_train, mse_test);
+		print('-depsc', cstrcat(OUTPUT_FOLDER, 'learning_curve_', SVR_DESCRIPTIONS{end}, '.eps'));
+		close(h);
+	end
 
 	models{end + 1} = model;
 	Cs(end + 1) = C;
@@ -301,7 +332,7 @@ end
 if ismember(3, MODELS_CHOSEN)
 	fprintf('\nTraining model with RBF SVR\n');
 	%fflush(stdout);
-	SVR_DESCRIPTIONS{end + 1} = 'Radial Basis Function SVR';
+	SVR_DESCRIPTIONS{end + 1} = 'SVR sigmoidale';
 
 	[C, eps] = model_selection (y_tr, X_tr, y_cv, X_cv, '-s 3 -t 2 -q -h 0', C_range, E_range);
 	options = ['-s 3 -t 2 -h 0 -p ', num2str(eps), ' -c ', num2str(C)];
@@ -309,6 +340,12 @@ if ismember(3, MODELS_CHOSEN)
 
 	[predictions(:, end + 1), accuracy, ~] = svmpredict (y_test, X_test, model, '-q');  %% quiet
 
+	if LEARNING_CURVES
+		[m, mse_train, mse_test] = learning_curves(y_tr, X_tr, y_test, X_test, [options, ' -q']);
+		h = plot_learning_curves(m, mse_train, mse_test);
+		print('-depsc', cstrcat(OUTPUT_FOLDER, 'learning_curve_', SVR_DESCRIPTIONS{end}, '.eps'));
+		close(h);
+	end
 
 	models{end + 1} = model;
 	Cs(end + 1) = C;
@@ -324,7 +361,7 @@ end
 %% Linear Regression
 
 if LINEAR_REGRESSION
-	fprintf('\nLinear regression:\n');
+	fprintf('\nTraining Linear regression.\n');
 
 	X_tr = [ones(N_train, 1) , X_tr]; %% Add the intercept
 
@@ -352,7 +389,7 @@ fd = -1;
 if SAVE_DATA
 	if ~ exist(OUTPUT_FOLDER)		%% Checks if the folder exists
 		if ~ mkdir(OUTPUT_FOLDER)		%% Try with the mkdir function
-			if system(cstrcat("mkdir -p ", OUTPUT_FOLDER))		%% This creates subfolders
+			if system(cstrcat('mkdir -p ', OUTPUT_FOLDER))		%% This creates subfolders
 				fprintf('[ERROR] Could not create output folder\nCreate the output folder first and then restart this script\n');
 				quit;
 			end
@@ -364,56 +401,60 @@ if SAVE_DATA
 
 	%% Prints train and test data location
 
-	fprintf(fd, "TRAIN DATA:\n");
+	fprintf(fd, 'TRAIN DATA:\n');
 	for index = 1:length(TRAIN_DATA_LOCATION)
-		fprintf(fd, "%s\n", TRAIN_DATA_LOCATION{index});
+		fprintf(fd, '%s\n', TRAIN_DATA_LOCATION{index});
 	end
 
-	fprintf(fd, "\n\nTEST DATA:\n");
+	fprintf(fd, '\n\nTEST DATA:\n');
 	for index = 1:length(TEST_DATA_LOCATION)
-		fprintf(fd, "%s\n", TRAIN_DATA_LOCATION{index});
+		fprintf(fd, '%s\n', TRAIN_DATA_LOCATION{index});
 	end
 
-	fprintf(fd, "\n\n\n");
+	fprintf(fd, '\n\n\n');
 end
 
-
-%% Compute metrics for all models
-
-for index = 1:length(MODELS_CHOSEN)
-	sum_abs = sum(abs(y_test .- predictions(:, index)));
-	sum_rel = sum(abs((y_test .- predictions(:, index)) ./ predictions(:, index)));
-	
-
-	mean_abs = sum_abs / N_test;
-	mean_rel = sum_rel / N_test;
-	fprintf('\n Testing results for %s:\n', SVR_DESCRIPTIONS{index});
-	fprintf('   RMSE = %f\n', RMSEs(index));
-	fprintf('   R^2 = %f\n', R_2(index));
-	fprintf('   Mean abs error = %f\n', mean_abs);
-	fprintf('   Mean rel error = %f\n', mean_rel);
-
-	if SAVE_DATA
-		fprintf(fd, '\n Testing results for %s:\n', SVR_DESCRIPTIONS{index});
-		fprintf(fd, '   RMSE = %f\n', RMSEs(index));
-		fprintf(fd, '   R^2 = %f\n', R_2(index));
-		fprintf(fd, '   Mean abs error = %f\n', mean_abs);
-		fprintf(fd, '   Mean rel error = %f\n', mean_rel);
-	end
-
-
-	y_mean = mean(y_test);
-	pred_mean = mean(predictions(:, index));
-	means(end + 1) = pred_mean;
-	if TEST_ON_CORES
-		diff_means = pred_mean - y_mean;
-		fprintf('   Difference between means = %f\n', diff_means);
-		if SAVE_DATA
-			fprintf(fd, '   Difference between means = %f\n', diff_means);
+if OUTPUT_LATEX
+	if ~ exist(OUTPUT_FOLDER)		%% Checks if the folder exists
+		if ~ mkdir(OUTPUT_FOLDER)		%% Try with the mkdir function
+			if system(cstrcat('mkdir -p ', OUTPUT_FOLDER))		%% This creates subfolders
+				fprintf('[ERROR] Could not create output folder\nCreate the output folder first and then restart this script\n');
+				quit;
+			end
 		end
 	end
-	fprintf('\n');
-end
+	latex_filename = strcat(OUTPUT_FOLDER, 'outputlatex.txt');
+	flatex = fopen(latex_filename, 'w');
+
+	fprintf(flatex, 'TRAIN DATA:\n');
+	for index = 1:length(TRAIN_DATA_LOCATION)
+		fprintf(flatex, '%s\n', TRAIN_DATA_LOCATION{index});
+	end
+
+	if ~isempty(TEST_DATA_LOCATION)
+		fprintf(flatex, '\n\nTEST DATA:\n');
+		for index = 1:length(TEST_DATA_LOCATION)
+			fprintf(flatex, '%s\n', TRAIN_DATA_LOCATION{index});
+		end
+	end
+
+	fprintf(flatex, '\n');
+
+	fprintf(flatex, cstrcat('\\begin{table}[bhpt]\n', ...
+					'\\centering\n', ...
+					'\\begin{adjustbox}{center}\n', ...
+					'\\begin{tabular}{c | c M{1cm} M{2.5cm} M{2.5cm} M{1.8cm}}\n'));
+	if TEST_ON_CORES
+		fprintf(flatex, 'Modello & RMSE & R\\textsuperscript{2} & Errore assoluto medio & Errore relativo medio & Differenza medie \\tabularnewline\n');
+	else
+		fprintf(flatex, 'Modello & RMSE & R\\textsuperscript{2} & Errore assoluto medio & Errore relativo medio \\tabularnewline\n');
+	end
+
+	fprintf(flatex, '\\hline\n');
+
+end	
+
+%% Compute metrics for all models
 
 if LINEAR_REGRESSION
 	y_mean = mean(y_test);
@@ -425,7 +466,7 @@ if LINEAR_REGRESSION
 
 	lin_RMSE = sqrt(sum_residual / N_test);			% Root Mean Squared Error
 	lin_R2 = 1 - (sum_residual / sum_total);		% R^2
-	lin_mean_abs = sum_abs / N_test;
+	lin_mean_abs = ((sum_abs / N_test) * sigma_y) + mu_y;
 	lin_mean_rel = sum_rel / N_test;
 
 	fprintf('\n Testing results for linear regression:\n');
@@ -442,8 +483,8 @@ if LINEAR_REGRESSION
 		fprintf(fd, '   Mean rel error = %f\n', lin_mean_rel);
 	end
 
-	RMSEs(end) = lin_RMSE;
-	R_2(end) = lin_R2;
+	RMSEs(end + 1) = lin_RMSE;
+	R_2(end + 1) = lin_R2;
 
 	pred_mean = mean(predictions(:, end));
 	means(end + 1) = pred_mean;
@@ -454,28 +495,101 @@ if LINEAR_REGRESSION
 			fprintf(fd, '   Difference between means = %f\n', diff_means);
 		end
 	end
+
+
+	if (OUTPUT_LATEX & ~TEST_ON_CORES)
+		fprintf(flatex, 'Regressione lineare & %5.4f & %5.4f & %6.0f & %5.4f \\\\\n', lin_RMSE, lin_R2, lin_mean_abs, lin_mean_rel);
+	end
+
+	if (OUTPUT_LATEX & TEST_ON_CORES)
+		fprintf(flatex, 'Regressione lineare & %5.4f & %5.4f & %6.0f & %5.4f & %5.4f \\\\\n', lin_RMSE, lin_R2, lin_mean_abs, lin_mean_rel, diff_means);
+	end
+
 	fprintf('\n');
 end
 
 
+for index = 1:length(MODELS_CHOSEN)
+	sum_abs = sum(abs(y_test .- predictions(:, index)));
+	sum_rel = sum(abs((y_test .- predictions(:, index)) ./ predictions(:, index)));
+	
+
+	mean_abs = ((sum_abs / N_test) * sigma_y) + mu_y;
+	mean_rel = sum_rel / N_test;
+	fprintf('\n Testing results for %s:\n', SVR_DESCRIPTIONS{index});
+	fprintf('   RMSE = %f\n', RMSEs(index));
+	fprintf('   R^2 = %f\n', R_2(index));
+	fprintf('   Mean abs error = %f\n', mean_abs);
+	fprintf('   Mean rel error = %f\n', mean_rel);
+
+	if SAVE_DATA
+		fprintf(fd, '\n Testing results for %s:\n', SVR_DESCRIPTIONS{index});
+		fprintf(fd, '   RMSE = %f\n', RMSEs(index));
+		fprintf(fd, '   R^2 = %f\n', R_2(index));
+		fprintf(fd, '   Mean abs error = %f\n', mean_abs);
+		fprintf(fd, '   Mean rel error = %f\n', mean_rel);
+	end
+
+	if OUTPUT_LATEX
+
+	end
+
+	y_mean = mean(y_test);
+	pred_mean = mean(predictions(:, index));
+	means(end + 1) = pred_mean;
+	if TEST_ON_CORES
+		diff_means = pred_mean - y_mean;
+		fprintf('   Difference between means = %f\n', diff_means);
+		if SAVE_DATA
+			fprintf(fd, '   Difference between means = %f\n', diff_means);
+		end
+	end
+
+	if (OUTPUT_LATEX & ~TEST_ON_CORES)
+		fprintf(flatex, '%s & %5.4f & %5.4f & %6.0f & %5.4f \\\\\n', SVR_DESCRIPTIONS{index}, RMSEs(index), R_2(index), mean_abs, mean_rel);
+	end
+
+	if (OUTPUT_LATEX & TEST_ON_CORES)
+		fprintf(flatex, '%s & %5.4f & %5.4f & %6.0f & %5.4f & %5.4f \\\\\n', SVR_DESCRIPTIONS{index}, RMSEs(index), R_2(index), mean_abs, mean_rel, diff_means);
+	end
+
+	fprintf('\n');
+end
+
+if OUTPUT_LATEX
+	fprintf(flatex, cstrcat('\\end{tabular}\n', ...
+					'\\end{adjustbox}\n', ...
+					'\\\\\n', ...
+					'\\caption{Risultati per il test su query ', QUERY, ' con datasize ', DATASIZE, '}\n', ...
+					'\\label{table_', QUERY, '_', DATASIZE, '}\n', ...
+					'\\end{table}\n'));
+
+	fprintf(flatex, cstrcat('\n\\begin {figure}[hbtp]\n', ...
+							'\\centering\n', ...
+							'\\includegraphics[width=\\textwidth]{', OUTPUT_FOLDER, 'plot_', QUERY, '_', DATASIZE, OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{2}, '}\n', ...
+							'\\caption {Plot per il test su query ', QUERY, ' con datasize ', DATASIZE, '}\n', ...
+							'\\end {figure}\n'));	
+	fclose(flatex);
+end
+
 %% Stores the context and closes the file descriptor
 if SAVE_DATA
-	fprintf(fd, "\n\n\n========================\n\n\n");
-	fprintf(fd, "ENABLE_FEATURE_FILTERING: %d\n", ENABLE_FEATURE_FILTERING);
-	fprintf(fd, "COMPLETION_TIME_THRESHOLD: %d\n", COMPLETION_TIME_THRESHOLD);
-	fprintf(fd, "TRAIN_FRAC_WO_TEST: %f\n", TRAIN_FRAC_WO_TEST);
-	fprintf(fd, "TEST_FRAC_WO_TEST: %f\n", TEST_FRAC_WO_TEST);
-	fprintf(fd, "TRAIN_FRAC_W_TEST: %f\n", TRAIN_FRAC_W_TEST);
-	fprintf(fd, "NORMALIZE_FEATURE: %d\n", NORMALIZE_FEATURE);
-	fprintf(fd, "CLEAR_OUTLIERS: %d\n", CLEAR_OUTLIERS);
-	fprintf(fd, "CHOOSE_FEATURES: %d\n", CHOOSE_FEATURES);
-	fprintf(fd, "FEATURES: %s --> ", mat2str(FEATURES));
+	fprintf(fd, '\n\n\n========================\n\n\n');
+	fprintf(fd, 'ENABLE_FEATURE_FILTERING: %d\n', ENABLE_FEATURE_FILTERING);
+	fprintf(fd, 'COMPLETION_TIME_THRESHOLD: %d\n', COMPLETION_TIME_THRESHOLD);
+	fprintf(fd, 'TRAIN_FRAC_WO_TEST: %f\n', TRAIN_FRAC_WO_TEST);
+	fprintf(fd, 'TEST_FRAC_WO_TEST: %f\n', TEST_FRAC_WO_TEST);
+	fprintf(fd, 'TRAIN_FRAC_W_TEST: %f\n', TRAIN_FRAC_W_TEST);
+	fprintf(fd, 'NORMALIZE_FEATURE: %d\n', NORMALIZE_FEATURE);
+	fprintf(fd, 'CLEAR_OUTLIERS: %d\n', CLEAR_OUTLIERS);
+	fprintf(fd, 'CHOOSE_FEATURES: %d\n', CHOOSE_FEATURES);
+	fprintf(fd, 'FEATURES: %s --> ', mat2str(FEATURES));
 	for id = 1:length(FEATURES)
-		fprintf(fd, "%s   ", FEATURES_DESCRIPTIONS{id});
+		fprintf(fd, '%s   ', FEATURES_DESCRIPTIONS{id});
 	end
-	fprintf(fd, "\n");
-	fprintf(fd, "TEST_ON_CORES: %d\n", TEST_ON_CORES);
-	fprintf(fd, "SHUFFLE_DATA: %d\n", SHUFFLE_DATA);
+	fprintf(fd, '\n');
+	fprintf(fd, 'TEST_ON_CORES: %d\n', TEST_ON_CORES);
+	fprintf(fd, 'SHUFFLE_DATA: %d\n', SHUFFLE_DATA);
 	
 
 	fclose(fd);
@@ -541,6 +655,9 @@ for col = 1:M
 		% 	scatter(X_test_denorm(1, col), means(end), 10, 'g', 'd', 'filled');
 		% end
 		plot(x_denorm, ylin, 'g', 'linewidth', 1);
+
+		save(cstrcat(OUTPUT_FOLDER, 'linear_regression_x.mat'), 'x_denorm');
+		save(cstrcat(OUTPUT_FOLDER, 'linear_regression_y.mat'), 'ylin');
 		
 
 	end
@@ -558,17 +675,19 @@ for col = 1:M
 		% end	
 		plot(x_denorm, ysvr, 'color', COLORS{index}, 'linewidth', 1);
 		
+		save(cstrcat(OUTPUT_FOLDER, SVR_DESCRIPTIONS{index}, '_x.mat'), 'x_denorm');
+		save(cstrcat(OUTPUT_FOLDER, SVR_DESCRIPTIONS{index}, '_y.mat'), 'ysvr');
 
 	end
 
 	% Plot the mean of the test values (for nCores)
-	if (ismember(13, FEATURES) & (col == M))
+	if (TEST_ON_CORES & ismember(13, FEATURES) & (col == M))
 		scatter(X_test_denorm(1, col), mean(y_test_denorm), 10, 'k', '.');		
 	end
 	
 	labels = {'Training set', 'Testing set'};
 	if LINEAR_REGRESSION
-		labels{end+1} = 'Linear regression';
+		labels{end+1} = 'Regressione lineare';
 	end
 	labels(end+1:end+length(SVR_DESCRIPTIONS)) = SVR_DESCRIPTIONS;
 	legend(labels, 'location', 'northeastoutside');
@@ -581,9 +700,16 @@ for col = 1:M
 	% title(cstrcat('Linear regression vs ', SVR_DESCRIPTIONS{svr_index})); 
 	if SAVE_DATA
 		% NOTE: the file location shouldn't have any spaces
-		file_location = strrep(strcat(OUTPUT_FOLDER, 'plot_', FEATURES_DESCRIPTIONS{col}, OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{2}), ' ', '');
+		% file_location = strrep(strcat(OUTPUT_FOLDER, 'plot_', FEATURES_DESCRIPTIONS{col}, OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{2}), ' ', '');
+		file_location = strrep(strcat(OUTPUT_FOLDER, 'plot_', QUERY, '_', DATASIZE, FEATURES_DESCRIPTIONS{col}, OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{2}), ' ', '');
 		print(OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{1}, file_location);
 	end
+
+	if SAVE_DATA & ismember(13, FEATURES) & (col == M)
+		file_location = strrep(strcat(OUTPUT_FOLDER, 'plot_', QUERY, '_', DATASIZE, OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{2}), ' ', '');
+		print(OUTPUT_FORMATS{PLOT_SAVE_FORMAT}{1}, file_location);
+	end
+
 	hold off;
 	
 	% pause;
